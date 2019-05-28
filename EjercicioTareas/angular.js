45bac6f4c3db07af117c5a548bea6e26928ebc7d
Alvaro <alvaromunozdominguez@gmail.com>
2019-05-28 17:00:57 +0200

    ejss

--- EjercicioTareas/angular.js
+++ EjercicioTareas/angular.js
@@ -14,49 +14,108 @@ app.controller('myCtrl', function ($scope, $filter) {
         "check": true
     }];
 
+
     // Por defecto, la vista de edicion
     // inhabilitada
-    $scope.noEditable = true
     $scope.modo;
+    $scope.noEditable = true
+    const labelNoAny = "Fecha de la tarea"
 
-    $scope.addTarea = function () {
-        $scope.noEditable=false
-        $scope.modo="crear"
+    $scope.Add = function () {
+        $scope.noEditable = false
+        $scope.modo = "crear";
     }
-    $scope.deleteTarea = function (index) {
+
+    $scope.Delete = function (index) {
         $scope.listaTareas.splice(index, 1)
     }
 
-    $scope.editTarea = function (index) {
-        $scope.noEditable=false
-        $scope.modo="editar"
+    $scope.Cancelar = function () {
+        // Volvemos a deshabilitarlo
+        $scope.noEditable = true
+        // Borramos los campos
+        $scope.valueAutor = ""
+        $scope.valueDesc = ""
+        $scope.valueFecha = ""
+        $scope.valueCheck = ""
+        $("label:eq(2)").text("Fecha de la tarea")
+        $("td a").show()
     }
-    
-    $scope.Cancelar = function (index) {
-        $scope.noEditable=true   
-        $scope.modelAutor = ""
-        $scope.modelDescripcion = ""
-        $scope.modelFecha = ""
-        $scope.modelCheck = ""
+    $scope.Editar = function (index) {
+        $scope.noEditable = false
+        $scope.modo = "editar";
+
+        // Guardamos la fila a la que hacemos referencia
+        // al editar
+        $scope.indiceEditar = index
+        var i = $scope.indiceEditar
+        // Pasamos lo valores actuales a la vista de 
+        // edicion
+        $scope.valueAutor = $scope.listaTareas[i].autor
+        $scope.valueDesc = $scope.listaTareas[i].desc
+        //$scope.valueFecha = $scope.listaTareas[i].fecha
+        $("label:eq(2)").text("Fecha original: " + $scope.listaTareas[i].fecha)
+        $scope.valueCheck = $scope.listaTareas[i].check
+
+        $("td a").hide()
+    }
+    $scope.Save = function () {
+        if ($scope.modo == "crear") {
+            $scope.valueFechaGG = $filter('date')($scope.valueFecha, "dd/MM/yyyy");
+            $scope.listaTareas.push({
+                "autor": $scope.valueAutor,
+                "desc": $scope.valueDesc,
+                "fecha": $scope.valueFechaGG,
+                "check": $scope.valueCheck
+            })
+            $("label:eq(2)").text("Fecha de la tarea")
+
+        } else {
+
+            // What else?
+            var i = $scope.indiceEditar
+            $scope.valueFechaGG = $filter('date')($scope.valueFecha, "dd/MM/yyyy");
+
+            $scope.listaTareas[i].autor = $scope.valueAutor
+            $scope.listaTareas[i].desc = $scope.valueDesc
+            if ($scope.valueFecha == undefined
+                || $scope.valueFechaGG == undefined
+                || $scope.valueFechaGG == ""
+                || $scope.valueFechaGG == null) {
+                // Any What else?
+            } else {
+                $scope.listaTareas[i].fecha = $scope.valueFechaGG
+            }
+            $scope.listaTareas[i].check = $scope.valueCheck
+
+            $scope.valueFechaGG = undefined
+            $("label:eq(2)").text("Fecha de la tarea")
+
+        }
+
+        // Volvemos a deshabilitarlo
+        $scope.noEditable = true
+        $scope.valueAutor = ""
+        $scope.valueDesc = ""
+        $scope.valueFecha = ""
+        $scope.valueCheck = ""
+        $("td a").show()
+        $("label:eq(2)").text("Fecha de la tarea")
+
+        /* angular.forEach($scope.listaTareas, function (key, index) {
+            console.log(key.autor)
+            console.log(key.check)
+            console.log(index)
+            if (key.check == true) {
+                $("tbody tr:eq(" + index + ")").css("background", "green")
+                console.log(index)
+            } else {
+                $("tbody tr:eq(" + index + ")").css("background", "yellow")
+            }
+        }) */
     }
 
-    $scope.save = function (index) {
-        if($scope.modo=="crear"){
-            $scope.fechaValor = $filter('date')($scope.modelFecha, "dd/MM/yyyy");
-            $scope.listaTareas.push({
-                "autor": $scope.modelAutor,
-                "desc": $scope.modelDescripcion,
-                "fecha": $scope.fechaValor,
-                "check": $scope.modelCheck
-            })
-        }
-        else{
-           
-        }
-        $scope.noEditable=true   
-        $scope.modelAutor = ""
-        $scope.modelDescripcion = ""
-        $scope.modelFecha = ""
-        $scope.modelCheck = ""
-    }
+
+
+
 });
